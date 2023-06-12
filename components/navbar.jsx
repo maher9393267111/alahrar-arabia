4fb6912c7478db0b426a854d6d903c519469b31b
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-06-12 20:54:15 +0300

    add fixed

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,7 +4,7 @@ import { whatsapp } from '../data/links';
 
 const Navbar = () => {
   return (
-    <nav className='py-4 border-b-2'>
+    <nav className='py-4 border-b-2 fixed top-0 left-0 right-0  bg-green-700 '>
       <Container className='flex justify-between items-center'>
         <div className='flex items-center '>
           <Link
@@ -19,7 +19,7 @@ const Navbar = () => {
         <div className='flex'>
           <Link
             href='https://www.google.com/maps/place/%D8%B4%D8%B1%D9%83%D8%A9+%D8%A7%D9%84%D8%A7%D8%AD%D8%B1%D8%A7%D8%B1+%D8%A7%D9%84%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9+AL-AHRAR+EXC%E2%80%AD/@41.0099031,28.9471323,17z/data=!3m1!4b1!4m6!3m5!1s0x14caba28d55e0171:0xcdb3c64334db17a7!8m2!3d41.0099031!4d28.9471323!16s%2Fg%2F11gbx9wddh?entry=ttu'
-            className='px-4 py-2 flex gap-1 text-sm sm:text-xl font-medium hover:font-bold'
+            className='px-4 py-2 items-center flex gap-1 text-[10px] sm:text-xl font-medium hover:font-bold'
           >
 
 
@@ -27,12 +27,17 @@ const Navbar = () => {
   <img className='w-8 h-8 object-cover' src="https://www.svgrepo.com/show/452221/google-maps.svg" alt="" />
 </span>
 
-        الحصول على الاتجاهات
+
+<p className=' '>
+
+الحصول على الاتجاهات
+</p>
+
 
           </Link>
           <Link
             href='https://wa.me/905319913648'
-            className='px-4 flex gap-1 py-2 text-sm sm:text-xl font-medium hover:font-bold'
+            className='px-4 flex items-center gap-1 py-2 text-[10px] sm:text-xl font-medium hover:font-bold'
           >
 
 <span>
@@ -41,9 +46,11 @@ const Navbar = () => {
 </span>
 
 
-{/* {whatsapp} */}
+<p >
+تواصل واتس اب
+</p>
  
-تواص واتس اب
+
 
           </Link>
           {/* <Link
